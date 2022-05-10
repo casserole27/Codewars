7455ceef23a8fc590b129d9d3e0c9f1c6937ea98
C Lewis <casserole.lewis@gmail.com>
2022-05-10 12:23:39 -0400

    Finished solution
    Explored how to split a number into a string and then turn it back into a number.

--- convernumberreversedarrayofdigits.js
+++ convernumberreversedarrayofdigits.js
@@ -2,6 +2,28 @@
 //Return the digits of this number within an array in reverse order
 
 function digitize(n) {
+ 
+  //define variable that makes the number into an array
+  //using the Array.from() method to create a new array instance
+  //defines n as a string to separate it into an array, and then a number
+  let numToArray = Array.from(String(n), Number);
+
+  //console.log(numToArray); //using this console log and playing with the .from() method, I was able to see how it was working.
+
+  //define a variable that holds the reversed array  
+  let reversedArray = numToArray.reverse();
+
+  //return the reversed array
+  return reversedArray;
+
+
+ //Codewars common solution
+ //In this case, the .map() method is turning the string back into a number.
+  //return String(n).split('').map(Number).reverse()
+ 
+
+
+  /*
     //convert number to a string and store the value in a variable
     let numToString = n.toString();
     
@@ -13,11 +35,12 @@ function digitize(n) {
 
     //return the array
     return reversedArray;
-
-    
+  */
   };
+
 
   console.log(digitize(35231));
   console.log(0);
 
   //Need to figure out how to ouput as numbers instead of strings
+  //Solution - string back to number using .from() or .map()
