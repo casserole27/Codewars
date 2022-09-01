b6cd0d2941f0798ac64b53960c1ce824a33eb705
C Lewis <casserole.lewis@gmail.com>
2022-09-01 07:06:21 -0400

    Added solution
    I added a simple and elegant solution from other users and wrote out how it works so that I understand that solution as well.

--- quarteroftheyear.js
+++ quarteroftheyear.js
@@ -21,7 +21,7 @@ const quarterOf = (month) => {
     }
     }; 
 
-console.log(quarterOf(8));  
+console.log(quarterOf(8));
 
 /*
 Given a month as an integer from 1 to 12, 
@@ -30,5 +30,15 @@ return to which quarter of the year it belongs as an integer number.
 For example: month 2 (February), is part of the first quarter; month 6 (June), 
 is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 */
+
+
+//Wow!!! Some users provided this solution, so simple and elegant.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
+//The Math.ceil() function always rounds a number up to the next largest integer. 
+//In this example, August is the 8th month, represented as the integer 8.
+//8/3 = 2.666667, Math.ceil would round this up to 3, which would place August in the 3rd quarter and return 3. 
+//For May, 5/3 = 1.66667, round up to 2, return 2 for 2nd quarter.
+const quarterOf = m => Math.ceil(m/3);
+console.log(quarterOf(8));
 
 
