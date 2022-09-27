4d5adf6018ed2871b9dd1d212792dd064ec1cd29
C Lewis <casserole.lewis@gmail.com>
2022-09-27 08:04:59 -0400

    Write out kata logic

--- closestelevator.js
+++ closestelevator.js
@@ -1,8 +1,32 @@
 
 function elevator(left, right, call){
     // code on! :)
-  }
+    if (left === right === call) {
+      return "right";
+    } else if (left === call || left === call + 1) {
+      return "left";
+    } else if (right === call || right === call + 1) { 
+      return "right";
+    }  
+}; 
+  
+console.log(elevator(0, 1, 0)); //left
+console.log(elevator(0, 1, 1)); //right
+console.log(elevator(0, 1, 2)); //right
+console.log(elevator(0, 0, 0)); //right
+console.log(elevator(0, 2, 1)); //right
 
+
+
+
+//? If call's number is closest to left number -> return left
+//? If call's number is closest to right number -> return right
+//? If left and right are equidistant from call -> return right left === right === call
+//? Inputs are always 0 - 2 
+
+//? Left can === 0, 1, 2
+//? Right can === 0, 1, 2
+//? Call can === 0, 1, 2
 
 /* Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
 
